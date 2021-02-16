7d5774ce3f147a14008f53c84532bce54a84b801
Ivanishcha <ivanishcha@gmail.com>
2021-02-16 10:10:47 +0300

    All done

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,30 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Ivanishcha <ivanishcha@gmail.com>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#ifndef TEMPLATE_HEADER_HPP
+#define TEMPLATE_HEADER_HPP
+#include <boost/filesystem.hpp>
+#include <vector>
+class Filesystem {
+public:
+    Filesystem(const std::string path_to_file);
+    void all_path(boost::filesystem::path p, std::ostream &out);
+    bool handler(boost::filesystem::path p, std::ostream &out);
+    bool check_fiilename(boost::filesystem::path p);
+    std::string what_account(std::string p);
+    std::string what_data(std::string p);
+    std::string what_broker(boost::filesystem::path p);
+    void show_account(std::ostream &out);
+    void insert_element(std::string account, std::string data, std::string broker);
+    friend std::ostream& operator<<(std::ostream &out, Filesystem& file_system);
+private:
+    boost::filesystem::path _path_to_ftp;
+    std::vector<std::string> _broker;
+    std::vector<std::string> _account;
+    std::vector<long int> _files;
+    std::vector<long int> _lastdate;
 
-#endif // INCLUDE_HEADER_HPP_
+    const size_t len_namefile = 29;
+    const std::string _numbers = "0123456789";
+};
+
+#endif  // TEMPLATE_HEADER_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,19 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Ivanishcha <ivanishcha@gmail.com>
 
 #include <gtest/gtest.h>
-
+#include "header.hpp"
 TEST(Example, EmptyTest) {
+    EXPECT_TRUE(true);
+}
+
+TEST(Programm, WorkTest) {
+    try {
+        std::string path("../misc/ftp");
+        Filesystem test(path);
+        std::cout << test;
+    }
+    catch(...) {
+
+    }
     EXPECT_TRUE(true);
 }
